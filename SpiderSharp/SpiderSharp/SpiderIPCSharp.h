3331da0d2bcd573dd921c371d72a5e4465ccaee2
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2021-05-07 15:35:48 +0900

    1. Cli프로젝트 추가

---
+++ SpiderSharp/SpiderSharp/SpiderIPCSharp.h
@@ -0,0 +1,43 @@
+#ifndef SPIDER_IPC_SHARP
+#define SPIDER_IPC_SHARP
+
+#include <SpiderIPC.h>
+
+using namespace System;
+using namespace System::Collections;
+using namespace System::Collections::Generic;
+using namespace System::Runtime::InteropServices;
+
+
+namespace SPIDER {
+
+	enum SPIDER_MODE {
+		CREATE = 0,
+		OPEN,
+	};
+
+	enum SPIDER_ACCESS {
+		READ_ONLY,
+		READ_WRITE
+	};
+
+	enum SPIDER_CALL_MODE {
+		SUBSCRIBER,
+		NOTIFIER
+	};
+	
+
+	class Variable {
+		private:
+			spider::variable<int> * instance;
+
+
+	public:
+		Variable() {
+			instance = new spider::variable<int>("test");
+		}
+	};
+
+}
+
+#endif
